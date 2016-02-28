d1f5f685cd790a7fe9e0cc773f72fa35eeda6c23
Andrew Sekula <andrew1980uk@gmail.com>
2016-02-28 22:03:49 +1100

    testing mail chimp form

--- js/init.js
+++ js/init.js
@@ -3,6 +3,23 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+
+
+    $("#mc-embedded-subscribe-form").submit(function(e){
+    	var $form = $(this);
+    	e.preventDefault();
+        $.ajax({
+            type     : "POST",
+            cache    : false,
+            url      : $(this).attr('action'),
+            data     : $(this).serialize(),
+            success  : function(data) {
+            	console.log(data);
+                //$(".printArea").empty().append(data).css('visibility','visible');
+            }
+        });
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
